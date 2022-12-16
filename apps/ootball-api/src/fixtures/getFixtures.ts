e380a365b18eb67d3aea088f7e6dfe7f84c5a815
stottle-uk <stuarttottle@hotmail.com>
2022-12-16 15:10:45 +0000

    trying dynamo

--- apps/ootball-api/src/fixtures/getFixtures.ts
+++ apps/ootball-api/src/fixtures/getFixtures.ts
@@ -1,3 +1,8 @@
+import {
+  DynamoDB,
+  DynamoDBClient,
+  ListTablesCommand,
+} from '@aws-sdk/client-dynamodb';
 import { APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda';
 
 const corsHeaders = {
@@ -26,8 +31,37 @@ export function httpResponse(
 }
 
 export const main: APIGatewayProxyHandler = async (event, context) => {
-  console.log('event', event);
-  console.log('context');
+  const client = new DynamoDBClient({ region: 'eu-west-1' });
+  const db = new DynamoDB({ region: 'eu-west-1' });
+  const command = new ListTablesCommand({});
+  try {
+    const results = await client.send(command);
+
+    const res = await db.putItem({
+      TableName: 'my-first-table',
+      Item: {
+        primaryKey: {
+          S: '1234',
+        },
+        otherData: {
+          M: {
+            data1: {
+              S: 'data1_val',
+            },
+            data2: {
+              S: 'data2_val',
+            },
+          },
+        },
+      },
+      ReturnValues: 'ALL_OLD',
+    });
+    console.log(res.Attributes);
+
+    console.log(results.TableNames.join('\n'));
+  } catch (err) {
+    console.error(err);
+  }
 
   return httpResponse({
     todo: [],
