21455cc272a7dbf34c075194e182f3acf2380503
stottle-uk <stuarttottle@hotmail.com>
2022-12-18 14:18:53 +0000

    try again

--- environments/environment.ts
+++ environments/environment.ts
@@ -3,7 +3,7 @@ import type { Environment } from './environment.types';
 export const env: Environment = {
   name: 'dev',
   region: 'eu-west-1',
-  profile: 'stottle',
+  profile: '',
   jwtSecret: 'secret',
   dynamo: {
     endpoint: 'http://localhost:4566',
