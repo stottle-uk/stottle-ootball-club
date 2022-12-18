47ab5fa2781d79dee7d891295c7b82a08a63b1ae
Stuart <stottle-uk@users.noreply.github.com>
2022-12-18 19:30:43 +0000

    started react router (#2)

--- apps/ootball-web/src/environments/environment.ts
+++ apps/ootball-web/src/environments/environment.ts
@@ -1,6 +1,7 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 
 export const environment = {
+  envName: 'dev',
   apiUrl: 'https://yxi58blahc.execute-api.eu-west-1.amazonaws.com/dev',
   production: false,
 };
--- apps/ootball-web/src/main.tsx
+++ apps/ootball-web/src/main.tsx
@@ -1,33 +1,46 @@
-import { StrictMode } from 'react';
+import React, { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './app/App';
 import { StateProvider } from './app/ootball/state/ootball.state';
+import { environment } from './environments/environment';
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type State = Record<string, any>;
+type MainFn = (
+  state: State,
+  router: ReturnType<typeof createBrowserRouter>
+) => () => React.ReactNode;
 
 declare global {
   interface Window {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    __CONFIG__?: Record<string, any>;
+    __CONFIG__?: State;
   }
 }
 
 const defaultState = ('__CONFIG__' in window && window.__CONFIG__) || {};
 
+const browserRouter = createBrowserRouter([
+  {
+    path: `/${environment.envName}/web-app`,
+    element: <App />,
+  },
+]);
+
+const getMainBuilder: MainFn = (state, router) => () =>
+  (
+    <StrictMode>
+      <StateProvider defaultState={state}>
+        <RouterProvider router={router} />
+      </StateProvider>
+    </StrictMode>
+  );
+
+const getMain = getMainBuilder(defaultState, browserRouter);
+
 const docRoot = document.getElementById('root') as HTMLElement;
 if (Object.keys(defaultState).length) {
-  ReactDOM.hydrateRoot(
-    docRoot,
-    <StrictMode>
-      <StateProvider defaultState={defaultState}>
-        <App />
-      </StateProvider>
-    </StrictMode>
-  );
+  ReactDOM.hydrateRoot(docRoot, getMain());
 } else {
-  ReactDOM.createRoot(docRoot).render(
-    <StrictMode>
-      <StateProvider defaultState={defaultState}>
-        <App />
-      </StateProvider>
-    </StrictMode>
-  );
+  ReactDOM.createRoot(docRoot).render(getMain());
 }
