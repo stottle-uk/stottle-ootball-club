eec1bd365310c11ca82bfd9ae320141245e0689a
stottle-uk <stuarttottle@hotmail.com>
2022-12-19 09:21:41 +0000

    set deplpy config

--- environments/environment.ts
+++ environments/environment.ts
@@ -3,7 +3,7 @@ import type { Environment } from './environment.types';
 export const env: Environment = {
   name: 'dev',
   region: 'eu-west-1',
-  profile: 'stottle',
+  profile: '',
   apiUrl: 'https://yxi58blahc.execute-api.eu-west-1.amazonaws.com/dev',
   bucketName: 'web-ssr-bucket-dev',
   bucketUrl: 'https://web-ssr-bucket-dev.s3.eu-west-1.amazonaws.com',
